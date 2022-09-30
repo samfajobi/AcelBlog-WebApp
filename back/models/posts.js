7a7cbaf3aa600cd152e131305dbce1df94c18c9f
Sam Fajobi <segzyfaj1@gmail.com>
2022-09-30 15:15:02 +0100

    Added Schema for Posts

---
+++ back/models/posts.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const postsSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        require: [true, "please provide a post title"]
+    },
+    desc: {
+        type: String,
+        required: [ true, "please provide a post description"]
+    },
+    username: {
+        type: String,
+        required: true
+    },  
+    photo: {
+        type: String,
+        required: [ true, "please provide a profile picture"]
+    },
+    categories: {
+        type: Array,
+        required: false
+    }
+ },  
+ {timeStamps: true}
+
+);
+
+
+module.exports = mongoose.model("postsModel", postsSchema)
